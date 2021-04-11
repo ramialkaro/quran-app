3c2edbb3d523717390a22a5176db45ded73b17ac
ramialkaro <rami.alkaro@gmail.com>
2021-04-11 17:20:01 +0300

    move the router out from tha app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
-import { Ayah } from "./components/Ayah";
-import { Azkar } from "./components/Azkar";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./theme";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import SideDrawer from "./components/SideDrawer";
 import { create } from "jss";
 import rtl from "jss-rtl";
 import { StylesProvider, jssPreset } from "@material-ui/core/styles";
+
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 
 function App() {
@@ -15,11 +14,7 @@ function App() {
     <StylesProvider jss={jss}>
       <ThemeProvider theme={theme}>
         <Router>
-          <Switch>
-            <Route exact path="/" component={SideDrawer} />
-            <Route path="/azkar" component={Azkar} />
-            <Route path="/sura/:id" component={Ayah} />
-          </Switch>
+          <SideDrawer />
         </Router>
       </ThemeProvider>
     </StylesProvider>
