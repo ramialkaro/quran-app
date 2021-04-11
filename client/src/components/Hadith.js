4c2e0154876a8217b28e4357d96b7e62333e7f26
ramialkaro <rami.alkaro@gmail.com>
2021-04-11 18:26:48 +0300

    hadith component

--- client/src/components/Hadith.js
+++ client/src/components/Hadith.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActionArea,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
@@ -14,9 +17,8 @@ const useStyles = makeStyles({
   },
 });
 
-export const Hadith = () => {
+export const Hadith = ({ hadith }) => {
   const classes = useStyles();
-
   return (
     <Card className={classes.root}>
       <CardActionArea>
@@ -29,16 +31,29 @@ export const Hadith = () => {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            {hadith.number}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+
+          <Typography
+            noWrap
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          >
+            {hadith.hadith}
           </Typography>
         </CardContent>
       </CardActionArea>
-     
+      <CardActions>
+        <Button
+          size="small"
+          component={Link}
+          to={`/hadith/${hadith.number}`}
+          color="primary"
+        >
+          اقرأ أكثر
+        </Button>
+      </CardActions>
     </Card>
   );
-}
-
+};
