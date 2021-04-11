33ed80f95f71221ac78eba5336f4b48aafafe24a
ramialkaro <rami.alkaro@gmail.com>
2021-04-11 17:20:31 +0300

    read the sura values from json file instead of the server

--- client/src/components/Ayah.js
+++ client/src/components/Ayah.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Container, Divider, Paper, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { apiFetch } from "../fetchData.js"
-import { names } from "../names"
-import { Link } from "react-router-dom"
+import { apiFetch } from "../fetchData.js";
+import { names } from "../names";
+import { Link } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
   root: {
-   backgroundColor: "#F0EFF4",
+    backgroundColor: "#F0EFF4",
     minHeight: "100vh",
     padding: theme.spacing(1),
   },
@@ -17,15 +17,15 @@ const useStyles = makeStyles((theme) => ({
     height: theme.spacing(16),
     color: "#3D2645",
     alignItems: "center",
-    backgroundColor:"white",
+    backgroundColor: "white",
     fontWeight: 300,
     fontSize: "22pt",
   },
-  back:{
-    textDecoration:"none",
-    color:"grey",
-    display:"flex",
-    justifyContent:"start",
+  back: {
+    textDecoration: "none",
+    color: "grey",
+    display: "flex",
+    justifyContent: "start",
   },
   ayah: {
     backgroundColor: "#F0EFF4",
@@ -42,41 +42,35 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const Ayah = ({ match }) => {
-    const classes = useStyles();
-    const [ sura, setSura] =  useState( null )
-  let {params: { id } } = match 
-  useEffect( () => {
-        apiFetch.get(`/sura/${id}`)
-          .then(res => res.data)
-        .then(data=> setSura( data))
-        .catch(err => console.error( err ))
+  const classes = useStyles();
+  let {
+    params: { id },
+  } = match;
+  let sura = require(`../data/Suras/surah_${id}.json`);
 
-      }, [])
   return (
     <Container maxWidth="xl" className={classes.root}>
-
-      <Grid  container elevation={0} className={classes.sura}>
-        <Grid item xs={1}> 
-        <Link className={classes.back} to="/">
-        عودة
-        </Link>
+      <Grid container elevation={0} className={classes.sura}>
+        <Grid item xs={1}>
+          <Link className={classes.back} to="/">
+            عودة
+          </Link>
         </Grid>
-        <Grid item container xs={11} justify="center"> 
-          {names[id-1]}
+        <Grid item container xs={11} justify="center">
+          {names[id - 1]}
+          <span>{sura.count}</span>
         </Grid>
       </Grid>
-    {
-      sura && sura.map((ayah, index) => {
-     
-        return (
-       <Paper key={index} elevation={0} className={classes.ayah}>
-         {ayah.AyahText}
-         <img src="Ayah.svg" alt="" width={50} height={50} />
-      </Paper>
-        ) } )
+      {sura &&
+        Object.values(sura.verse).map((verse) => {
+          return (
+            <Paper elevation={0} className={classes.ayah}>
+              {verse}
+              <img src="Ayah.svg" alt="" width={50} height={50} />
+            </Paper>
+          );
+        })}
 
-    }
-       
       <Divider />
     </Container>
   );
