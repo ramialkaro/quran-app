7aeeabddaaf7f68caa0fa116ef0795c887f87f3e
ramialkaro <rami.alkaro@gmail.com>
2021-04-11 17:22:18 +0300

    add the hadith view container

---
+++ client/src/views/HadithPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Hadith } from '../components/Hadith';
+import { Container } from "@material-ui/core";
+export const HadithPage = () => {
+    return (
+        <Container>
+           <Hadith/> 
+        </Container>
+    )
+}
